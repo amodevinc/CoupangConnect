4a66a23f2df039481a236231480f6998033cdbc5
ganyunhee <eunicenellgan@gmail.com>
2024-08-25 00:26:52 +0900

    Add animated Lottifile iframes

--- frontend/src/Loading.js
+++ frontend/src/Loading.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Wrapper} from './styledComponents';
-import icon from './assets/loading.svg';
+//import icon from './assets/loading.svg';
 
 const Loading = () => {
     return (
         <Container>
             <Wrapper>
                 <header className="Loading-header">
-                    <img src={icon} className="Loading-logo" alt="Loading. Please wait..." />
+                    <iframe src="https://lottie.host/embed/c52c5c13-24f1-4cba-bbf6-ac06d5e2d2f9/sgYQV51Jmu.json"></iframe>
+                    {/*<img src={icon} className="Loading-logo" alt="Loading. Please wait..." />*/}
                     <caption>Opening ...</caption>
                 </header>
             </Wrapper>
--- frontend/src/PaymentSuccess.js
+++ frontend/src/PaymentSuccess.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Button, Wrapper, ButtonWrapper } from './styledComponents';
-import icon from './assets/success.svg';
+//import icon from './assets/success.svg';
 
 const PaymentSuccess = () => {
     return (
         <Container>
             <Wrapper>
                 <header className="Success-header">
-                    <img src={icon} className="Success-logo" alt="Payment Success!" />
+                    <iframe src="https://lottie.host/embed/c19a8294-5e97-4204-a194-eac78344e75d/aR6uHy0CYh.json"></iframe>
+                    {/*<img src={icon} className="Success-logo" alt="Payment Success!" />*/}
                     <h2>Payment Successful</h2>
                 </header>
             </Wrapper>
