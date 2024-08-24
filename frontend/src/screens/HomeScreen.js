935ae196decbd5eb072540fed4e933cdad297b2c
alainadaviv <alain@adaviv.com>
2024-08-24 19:59:34 +0900

    fix: invalid import

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Navigation from '../components/Navigation';
 import ProductGallery from '../components/ProductGallery';
 import CreateSharedCartModal from '../modals/CreateSharedCartModal';
 import { useSharedCart } from '../hooks/useSharedCart';
